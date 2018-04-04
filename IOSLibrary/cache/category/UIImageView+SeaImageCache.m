20ef529b2346f1b46c9921233864eef2c75d98cb
Hank <Hank@aizhongjiedeMacBook-Air.local>
2018-04-04 16:31:59 +0800

    更新

--- IOSLibrary/cache/category/UIImageView+SeaImageCache.m
+++ IOSLibrary/cache/category/UIImageView+SeaImageCache.m
@@ -7,8 +7,21 @@
 //
 
 #import "UIImageView+SeaImageCache.h"
+#import <objc/runtime.h>
 
 @implementation UIImageView (SeaImageCache)
+
+- (void)setSea_thumbnailSize:(CGSize)sea_thumbnailSize
+{
+    objc_setAssociatedObject(self, &SeaImageCacheToolThumbnailSize, [NSValue valueWithCGSize:sea_thumbnailSize], OBJC_ASSOCIATION_RETAIN);
+}
+
+- (CGSize)sea_thumbnailSize
+{
+    NSValue *value = objc_getAssociatedObject(self, &SeaImageCacheToolThumbnailSize);
+
+    return [value CGSizeValue];
+}
 
 - (void)sea_setLoading:(BOOL) loading options:(SeaImageCacheOptions*) options
 {
